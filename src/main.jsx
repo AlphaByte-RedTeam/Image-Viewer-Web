5e27d3ea81ddacf3fa1f1eb73f2d6dd8aba4746b
Chandra Medhika <chandra163.cm@gmail.com>
2023-11-18 21:46:49 +0700

    "update code in main.jsx and App.jsx"

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, BrowserRouter,Routes,Route } from 'react-router-dom'
 import './index.css'
 
 const router = createBrowserRouter([
@@ -14,6 +14,12 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={ <App /> }>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )

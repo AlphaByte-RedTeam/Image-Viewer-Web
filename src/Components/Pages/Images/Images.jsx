48928cf69328396bf31809d5e8771cba1cf9ee0a
Chandra Medhika <chandra163.cm@gmail.com>
2023-12-06 13:12:46 +0700

    create virtualizer

--- src/Components/Pages/Images/Images.jsx
+++ src/Components/Pages/Images/Images.jsx
@@ -1,60 +1,98 @@
-import Navbar from "../../Navbar/Navbar";
-import { useVirtualizer } from "@tanstack/react-virtual";
+import React from 'react';
+import { useVirtualizer } from '@tanstack/react-virtual';
+import { For } from 'million/react'
 
 const Images = () => {
+    const parentRef = React.useRef(null);
 
-    const modules = import.meta.glob('/src/assets/images/*.{png,jpe?g,jpg}');
-    const gallery = []
-    for (const path in modules) {
-        const p = new URL(path, import.meta.url).href;
-        gallery.push(p);
-    }
-    console.log(gallery)
-
-    const renderItem = ({ image }) => (
-        <a href={image} target="_blank">
-            <img src={image} />
-        </a>
-    );
-
-    //const virtualizer = useVirtualizer(gallery, renderItem);
-    const virtualizer = useVirtualizer(gallery, renderItem, {
-        observeElementOffset: true,
-        offset: ({ element }) => {
-            return {
-                top: element.getBoundingClientRect().top,
-                left: element.getBoundingClientRect().left,
-            };
-        },
+    const imageModules = import.meta.glob('/src/assets/images/*.{png,jpe?g,jpg}');
+    const gallery = Object.keys(imageModules).map((path) => {
+        const imageUrl = new URL(path, import.meta.url).href;
+        return imageUrl;
     });
+    const imgCount = gallery.length;
 
+    const rowVirtualizer = useVirtualizer({
+        count: imgCount,
+        estimateSize: () => 100,
+        getScrollElement: () => parentRef.current,
+        // debug: true,
+        // horizontal: true,
+        // measureElement: async (el, instance) => {
+        //     if (el) {
+        //         // Sesuaikan pengukuran sesuai dengan kebutuhan, Mengukur elemen menggunakan getBoundingClientRect()
+        //         const rect = el.getBoundingClientRect();
+        //         const height = rect.height;
+
+        //         // Set ukuran yang diukur ke virtualizer.
+        //         instance.resizeItem(el.dataset.index, height);
+        //     }
+        // },
+    })
+
+    const virtualItems = rowVirtualizer.getVirtualItems();
 
     return (
-        <div>
-            <Navbar />
-            <br />
-            <h3 className="text-3xl text-white underline font-bold text-center">
-                Images Gallery
-            </h3>
-            <br />
-            <br />
-            {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {gallery.map((image, index) => (
-                    <a href={image} target="_blank">
-                        <img src={image} />
+        <div ref={parentRef} className="flex flex-row gap-4">
+            <For
+                each={virtualItems}
+                style={{
+                    height: `${rowVirtualizer.getTotalSize()}px`,
+                    width: '100%',
+                    position: 'relative',
+                }}
+                as="div"
+            >
+                {(item) => (
+                    <div
+                        key={item.key}
+                        style={
+                            {
+                                position: 'absolute',
+                                top: 0,
+                                left: 0,
+                                width: '100%',
+                                fontSize: '20px',
+                                height: `${item.size}px`,
+                                transform: `translateY(${item.start}px)`,
+                            }
+                        }
+                    >
+                        Image {item.index}
+                        {/* <a href={item.key} target="_blank" rel="noopener noreferrer">
+                            <img
+                                src={item.key}
+                                alt={`Image ${item.index}`}
+                            />
+                        </a> */}
+                    </div>
+                )}
+            </For>
+            {/* {virtualItems.map((virtualItem) => (
+                <div
+                    key={virtualItem.index}
+                    data-index={virtualItem.index}
+                    ref={rowVirtualizer.ref}
+                    style={{
+                        position: 'relative',
+                        width: '100%',
+                        height: rowVirtualizer.getTotalSize(),
+                        transform: `translateY(${virtualItem.start}px)`,
+                    }}
+                >
+                    <a href={virtualItem.key} target="_blank" rel="noopener noreferrer">
+                        <img
+                            src={virtualItem.key}
+                            alt={`Image ${virtualItem.index}`}
+                            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                        />
                     </a>
-                ))}
-            </div> */}
-
-
-            <ul>
-                {virtualizer()}
-            </ul>
-
+                </div>
+            ))} */}
 
         </div>
     );
-}
+};
 
-export default Images
+export default Images;
 
